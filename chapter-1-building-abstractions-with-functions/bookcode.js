33a4452de4bcc34e41c2f63a4dc3fabc82fe0fdc
terrence-ou <outerrencedl@gmail.com>
2024-03-13 00:09:35 -0400

    chapter 1 book code added

---
+++ chapter-1-building-abstractions-with-functions/bookcode.js
@@ -0,0 +1,22 @@
+// 1.1.7 Example: Square Roots by Newton's Method
+function sqrt_iter(guess, x) {
+  return is_good_enough(guess, x) ? guess : sqrt_iter(improve(guess, x), x);
+}
+
+function is_good_enough(guess, x) {
+  return Math.abs(guess ** 2 - x) < 1e-3;
+}
+
+function improve(guess, x) {
+  return average(guess, x / guess);
+}
+
+function average(x, y) {
+  return (x + y) / 2;
+}
+
+function sqrt(x) {
+  return sqrt_iter(1, x);
+}
+
+console.log(sqrt(9));
